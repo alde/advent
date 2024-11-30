72f041886420762a797551c5b319e015d443c3e6
Rickard Dybeck <r.dybeck@gmail.com>
2024-11-30 10:32:57 -0500

    fix the entrypoint

--- 2024/go/main.go
+++ 2024/go/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"strconv"
 
 	"alde.nu/advent/2024/day1"
@@ -12,8 +13,8 @@ func main() {
 	target := flag.String("target", "all", "specify which day to run, or all")
 	flag.Parse()
 
-	days := []func(){
-		day1.Solve,
+	days := map[int]func(){
+		1: day1.Solve,
 	}
 
 	if *target == "all" {
@@ -25,8 +26,14 @@ func main() {
 
 	day, err := strconv.Atoi(*target)
 	if err != nil {
-		fmt.Printf("day must be either 'all' or an integer. Got %+v", target)
-	} else {
-		days[day]()
+		fmt.Printf("day must be either 'all' or an integer. Got '%v'\n", *target)
+		os.Exit(2)
 	}
+
+	dayFunc, ok := days[day]
+	if !ok {
+		fmt.Printf("day %d has not been added to the list of solved days\n", day)
+		os.Exit(2)
+	}
+	dayFunc()
 }
