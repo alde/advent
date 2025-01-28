76a1c3729f2672aefb344d5fae3e5eba35731031
Rickard Dybeck <r.dybeck@gmail.com>
2025-01-28 16:53:53 -0500

    [zig] 2015 - day 6 part 2

--- 2015/zig/src/day6/main.zig
+++ 2015/zig/src/day6/main.zig
@@ -35,6 +35,38 @@ pub fn part1() !void {
     try stdout.print("Result: {}\n", .{result});
 }
 
+pub fn part2() !void {
+    try stdout.print("Day 6 Part 2\n", .{});
+    var line_it = try shared.iterLines("src/day6/input.txt");
+    defer line_it.deinit();
+    const n: usize = 1000;
+
+    // Need to create the matrix on the heap because it's too big for the stack.
+    // This is not apparent because `zig build` doesn't say anything - it just
+    // crashes with a exit code 1 and no stack trace.
+    const allocator = std.heap.page_allocator;
+
+    const temp = try allocator.alloc([n][n]usize, 1);
+    defer allocator.free(temp);
+
+    // Initialize the slice
+    for (&temp[0]) |*row| {
+        for (row) |*val| {
+            val.* = 0;
+        }
+    }
+    var matrix: [n][n]usize = temp.ptr[0];
+
+    while (try line_it.next()) |line| {
+        const is = try processInstruction(line);
+        // stdout.print("is.action {}\n", .{is.action}) catch unreachable;
+        updateMatrix2(n, n, &matrix, is);
+    }
+    const result = countLights(n, n, matrix);
+
+    try stdout.print("Result: {}\n", .{result});
+}
+
 const Action = enum { TURN_ON, TOGGLE, TURN_OFF };
 const Point = struct {
     x: usize,
@@ -135,6 +167,20 @@ fn updateMatrix(comptime X: usize, comptime Y: usize, mat: *[X][Y]usize, is: Ins
     }
 }
 
+fn updateMatrix2(comptime X: usize, comptime Y: usize, mat: *[X][Y]usize, is: InstructionSet) void {
+    for (is.start.x..(is.end.x + 1)) |x| {
+        for (is.start.y..(is.end.y + 1)) |y| {
+            switch (is.action) {
+                Action.TURN_ON => mat[x][y] += 1,
+                Action.TURN_OFF => {
+                    if (mat[x][y] > 0) mat[x][y] -= 1 else mat[x][y] = 0;
+                },
+                Action.TOGGLE => mat[x][y] += 2,
+            }
+        }
+    }
+}
+
 fn countLights(comptime X: usize, comptime Y: usize, mat: [X][Y]usize) usize {
     var count: usize = 0;
     for (mat) |row| {
@@ -166,8 +212,41 @@ test "updateMatrix" {
     {
         var mat = [3][3]usize{ .{ 0, 0, 0 }, .{ 0, 0, 0 }, .{ 0, 0, 0 } };
         const expected = [3][3]usize{ .{ 1, 1, 1 }, .{ 0, 0, 0 }, .{ 0, 0, 0 } };
-        const is: InstructionSet = InstructionSet{ .action = Action.TOGGLE, .start = Point{ .x = 0, .y = 0 }, .end = Point{ .x = 0, .y = 3 } };
+        const is: InstructionSet = InstructionSet{ .action = Action.TOGGLE, .start = Point{ .x = 0, .y = 0 }, .end = Point{ .x = 0, .y = 2 } };
         updateMatrix(3, 3, &mat, is);
+        try testing.expectEqual(
+            expected,
+            mat,
+        );
+    }
+}
+
+test "updateMatrix2" {
+    {
+        var mat = [3][3]usize{ .{ 0, 0, 0 }, .{ 0, 0, 0 }, .{ 0, 0, 0 } };
+        const expected = [3][3]usize{ .{ 1, 1, 1 }, .{ 1, 1, 1 }, .{ 1, 1, 1 } };
+        const is: InstructionSet = InstructionSet{ .action = Action.TURN_ON, .start = Point{ .x = 0, .y = 0 }, .end = Point{ .x = 2, .y = 2 } };
+        updateMatrix2(3, 3, &mat, is);
+        try testing.expectEqual(
+            expected,
+            mat,
+        );
+    }
+    {
+        var mat = [3][3]usize{ .{ 0, 0, 0 }, .{ 0, 0, 0 }, .{ 0, 0, 0 } };
+        const expected = [3][3]usize{ .{ 2, 2, 2 }, .{ 0, 0, 0 }, .{ 0, 0, 0 } };
+        const is: InstructionSet = InstructionSet{ .action = Action.TOGGLE, .start = Point{ .x = 0, .y = 0 }, .end = Point{ .x = 0, .y = 2 } };
+        updateMatrix2(3, 3, &mat, is);
+        try testing.expectEqual(
+            expected,
+            mat,
+        );
+    }
+    {
+        var mat = [3][3]usize{ .{ 2, 2, 2 }, .{ 1, 1, 1 }, .{ 0, 0, 0 } };
+        const expected = [3][3]usize{ .{ 1, 1, 1 }, .{ 0, 0, 0 }, .{ 0, 0, 0 } };
+        const is: InstructionSet = InstructionSet{ .action = Action.TURN_OFF, .start = Point{ .x = 0, .y = 0 }, .end = Point{ .x = 2, .y = 2 } };
+        updateMatrix2(3, 3, &mat, is);
         try testing.expectEqual(
             expected,
             mat,
@@ -184,13 +263,7 @@ test "countLights" {
 
     const mat3 = [3][3]usize{ .{ 0, 0, 0 }, .{ 1, 0, 1 }, .{ 0, 0, 0 } };
     try testing.expectEqual(2, countLights(3, 3, mat3));
+
+    const mat4 = [3][3]usize{ .{ 2, 2, 2 }, .{ 1, 0, 1 }, .{ 0, 0, 0 } };
+    try testing.expectEqual(8, countLights(3, 3, mat4));
 }
-
-pub fn part2() !void {
-    try stdout.print("Day 6 Part 2\n", .{});
-    const result: i32 = 0;
-
-    try stdout.print("Result: {}\n", .{result});
-}
-
-test "day 6 part two" {}
